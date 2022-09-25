fc4cd98f435a659558570efed6f92c45aff6e98e
ChairmanArtgineer <mr.blackcloud911@gmail.com>
2022-09-24 22:06:30 -0500

    //MAIN  >corrected some bugs with the constains addon
    >added Pose mode visibility only for the addon (to prevent some runetime errors poppin on the user"
    
    > added a new experimental button for settin parents on pose mode. user must be aware that pose transfotms must be 0, it may be ideal for reparenting control bones and prob not deform bones, use it at your own risk
    
    ///AESTHETIC CHANGES
    
    added new row and column description to keep buttons organized depending their functions

--- __init__.py
+++ __init__.py
@@ -11,22 +11,34 @@ import bpy
 from .mixamorenamer import RENAME_OT_Mtob
 from .mixamorenamer import RENAME_OT_Btom
 from .constains import RemoveConsOperator
+from .parenting import ParentPoseMode
 
 
 
-
-class VIEW3D_UI(bpy.types.Panel):
+class VIEW3D_main_UI(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = "anti autodesk tools"
     bl_label = "very unorthodox rigg tools"
     bl_order = 0
-
     def draw(self, context):
-        self.layout.operator('remove.all_constrains',
-                             icon = 'CONSTRAINT_BONE')
-        pass
-class VIEW3D_UI2(bpy.types.Panel):
+        if (bpy.context.mode == 'POSE'):
+            layout = self.layout
+
+            row = layout.row()
+            col = layout.column(align=True)
+            row.label(text="constrains", translate=False)
+            col.operator('remove.all_constrains',
+                                 icon = 'CONSTRAINT_BONE')
+
+            row = layout.row()
+            col = layout.column(align=True)
+            row.label(text="parenting", translate=False)
+            col.operator('parent.posemode',
+                                icon = 'BONE_DATA')
+            col.prop(context.scene, 'ConnectBones')
+            pass
+class VIEW3D_MixamoUI(bpy.types.Panel):
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = "anti autodesk tools"
@@ -34,31 +46,35 @@ class VIEW3D_UI2(bpy.types.Panel):
     bl_order = 1
 
     def draw(self, context):
-        self.layout.operator('rename.mixtoblend',
-                             icon = 'BLENDER')
-        self.layout.operator('rename.blendtomix',
-                             icon = 'EVENT_M')
-        pass
+        if (bpy.context.mode == 'POSE'):
+            self.layout.operator('rename.mixtoblend',
+                                 icon = 'BLENDER')
+            self.layout.operator('rename.blendtomix',
+                                 icon = 'EVENT_M')
+            pass
 
+
+BlenderClasses = [
+VIEW3D_main_UI,
+VIEW3D_MixamoUI,
+RENAME_OT_Mtob,
+RENAME_OT_Btom,
+RemoveConsOperator,
+ParentPoseMode
+]
 
 def register():
-    bpy.utils.register_class(VIEW3D_UI)
-    bpy.utils.register_class(VIEW3D_UI2)
-    bpy.utils.register_class(RENAME_OT_Mtob)
-    bpy.utils.register_class(RENAME_OT_Btom)
-    bpy.utils.register_class(RemoveConsOperator)
-
-    print("hello world")
-
+    for blender_class in BlenderClasses:
+        bpy.utils.register_class(blender_class)
 
 def unregister():
-    bpy.utils.unregister_class(VIEW3D_UI)
-    bpy.utils.unregister_class(VIEW3D_UI2)
-    bpy.utils.unregister_class(RENAME_OT_Mtob)
-    bpy.utils.unregister_class(RENAME_OT_Btom)
-    bpy.utils.unregister_class(RemoveConsOperator)
+    del bpy.types.Scene.ConnectBones
+    for blender_class in BlenderClasses:
+        bpy.utils.unregister_class(blender_class)
 
     print("hello world")
+
+
 
 if __name__ == "__main__":
     register()
---
+++ parenting.py
@@ -0,0 +1,43 @@
+import bpy
+
+
+
+
+def parentingfunction(setconnected):
+
+    slct_bones = bpy.context.selected_pose_bones
+    act_bone = bpy.context.active_pose_bone
+    if(slct_bones is not None):
+        bpy.ops.object.mode_set(mode='EDIT')
+        slct_bones = bpy.context.selected_bones
+        act_bone = bpy.context.active_bone
+
+    for bone in slct_bones:
+
+        if act_bone.name == bone.name:
+            pass
+        else:
+            bone.parent = act_bone
+            bone.use_connect = setconnected
+    bpy.ops.object.mode_set(mode='POSE')
+
+
+class ParentPoseMode(bpy.types.Operator):
+    """sets bone parents from pose mode"""
+    bl_idname = "parent.posemode"
+    bl_label = "parent from posemode"
+
+    bpy.types.Scene.ConnectBones = bpy.props.BoolProperty(
+        name='Connected')
+    def execute(self, context):
+        parentingfunction(context.scene.ConnectBones)
+
+        return {'FINISHED'}
+
+
+
+
+
+
+
+
